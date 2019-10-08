7a9e93837d0d72417482f9a5f62d072ff98ab3bf
Old <old@forgetlooks.com>
2019-10-08 12:40:34 -0700

    finished chapter 3 times... and changed it to 6 seconds to speed itup

--- app/src/main/java/com/example/timefigher/MainActivity.kt
+++ app/src/main/java/com/example/timefigher/MainActivity.kt
@@ -14,6 +14,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tapMeButton: Button
     private var score = 0
 
+    private var gameStarted = false
+    private lateinit var countDownTimer: CountDownTimer
+    private var initialCountDOwn: Long = 6000
+    private var countDownInterval : Long = 1000
+    private var timeLeft = 6
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,20 +32,58 @@ class MainActivity : AppCompatActivity() {
             incrementScore()
         })
 
+        resetGame()
+
     }
 
     private fun incrementScore() {
+        if (!gameStarted){
+            startGame()
+        }
+
+
         score++
-        val newScore = "Your Score: $score"
+        val newScore = getString(R.string.your_score,score)
         gameScoreTextview.text = newScore
 
     }
 
     private fun resetGame(){
+        score = 0
+        val initialScore = getString(R.string.your_score, score)
+        gameScoreTextview.text = initialScore
+
+        val initialTimeLeft = getString(R.string.time_left,timeLeft)
+        timeLeftTextView.text = initialTimeLeft
+
+        countDownTimer = object : CountDownTimer(initialCountDOwn,countDownInterval){
+
+            //overrides functionality so we see it tick down then passes it to the view
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeft = millisUntilFinished.toInt()/1000
+                val timeLeftString = getString(R.string.time_left, timeLeft)
+                timeLeftTextView.text = timeLeftString
+            }
+
+            override fun onFinish() {
+                endGame()
+            }
+
+        }
+
+        gameStarted = false
+
+    }
+
+    private fun startGame() {
+        countDownTimer.start()
+        gameStarted = true
 
     }
 
     private fun endGame(){
+        Toast.makeText(this, getString(R.string.game_over_message, score), Toast.LENGTH_LONG).show()
+        resetGame()
 
     }
 
